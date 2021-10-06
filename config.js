2af5f71807021ff2f8200a450dcbd87192b3b3a9
prempandeyCS <prem.pandey@crownstack.com>
2021-10-06 19:54:41 +0530

    weather app

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "883307c0c07ee1f314de792a4448a40e"
+    }
+}
+
+module.exports =constants;
---
+++ utils/weatherData.js
@@ -0,0 +1,22 @@
+const request = require ('request');
+const constants = require('../config');
+
+const weatherData = (address, callback) =>{
+    const url= constants.openWeatherMap.BASE_URL + encodeURIComponent(address) + '&appid=' + constants.openWeatherMap.SECRET_KEY;
+    console.log(url);
+    request({url, json:true}, (error,{body}) =>{
+        
+if (error) {
+    callback('can not fetch data from open weather map api', undefined)
+} else{
+    callback(undefined, {
+        temperature: body.main.temp,
+        description: body.weather[0].description,
+        cityName: body.name
+    })
+}
+
+    })
+}
+
+module.exports = weatherData;
